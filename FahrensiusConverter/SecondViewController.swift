bb1d5884eede8869830c777b75d0429bcda13b91
Sébastien Gaya <somniia.99@gmail.com>
2017-11-13 14:24:08 +0100

    First commit

---
+++ FahrensiusConverter/SecondViewController.swift
@@ -0,0 +1,107 @@
+//
+//  SecondViewController.swift
+//  FahrensiusConverter
+//
+//  Created by Sébastien Gaya on 13/11/2017.
+//  Copyright © 2017 Sébastien Gaya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var userValueField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var unitControl: UISegmentedControl!
+    @IBOutlet weak var resultView: UIView!
+    
+    // MARK: Actions
+    
+    @IBAction func changeUnitControl(_ sender: UISegmentedControl) {
+        processConversion()
+    }
+    
+    // MARK: UITextFieldDelegate
+    
+    @objc func userValueDidChange(_ userValueField: UITextField) {
+        processConversion()
+    }
+    
+    // MARK: Conversion Methods
+    
+    func processConversion() {
+        guard let userValue = Float(userValueField.text!) else {
+            if userValueField.text == "" {
+                resultLabel.text = "Pas de valeur entrée."
+            } else {
+                resultLabel.text = "Valeur incorrecte."
+            }
+            return
+        }
+        
+        let userUnit = unitControl.selectedSegmentIndex
+        
+        var resultValue: Float = 0.0
+        
+        switch userUnit {
+        case 0:
+            resultValue = toFahrenheit(from: userValue)
+            resultLabel.text = resultValue.description + " °F"
+            break
+        case 1:
+            resultValue = toCelsius(from: userValue)
+            resultLabel.text = resultValue.description + " °C"
+            break
+        default:
+            resultLabel.text = "Unité incorrecte"
+        }
+    }
+    
+    func toFahrenheit(from celsiusValue: Float) -> Float {
+        // Renvoie la valeur convertie en degrés Fahrenheit
+        return celsiusValue * 1.8 + 32
+    }
+    
+    func toCelsius(from fahrenheitValue: Float) -> Float {
+        // Renvoie la valeur convertie en degrés Celsius
+        return (fahrenheitValue - 32) / 1.8
+    }
+    
+    
+    // MARK: Override Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        userValueField.addTarget(self, action: #selector(userValueDidChange(_:)), for: .editingChanged)
+        
+        // Set border to result UIView
+        resultView.layer.borderColor = UIColor.black.cgColor
+        resultView.layer.borderWidth = 1
+        
+        // Set default text to empty value
+        resultLabel.text = ""
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FahrensiusConverter/ViewController.swift
+++ FahrensiusConverter/ViewController.swift
@@ -8,11 +8,104 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
 
+    // MARK: Properties
+    let pickerValues = ["Celsius => Fahrenheit", "Fahrenheit => Celsius"]
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var userValueField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var userDirectionPicker: UIPickerView!
+    @IBOutlet weak var resultView: UIView!
+    
+    
+    // MARK: Actions
+    
+    @IBAction func convertBtn(_ sender: UIButton) {
+        
+        // Vérifie que la valeur entrée est un nombre décimal
+        guard let userValue = Float(userValueField.text!) else {
+            resultLabel.text = "Valeur incorrecte."
+            return
+        }
+        
+        // Vérifie que le sens de direction est bien choisi
+        let selectedDirection = userDirectionPicker.selectedRow(inComponent: 0)
+        if selectedDirection == -1 {
+            resultLabel.text = "Sens de conversion incorrect."
+        }
+        
+        // Initialisation du résultat
+        var resultValue: Float = 0.0
+        
+        switch selectedDirection {
+        case 0:
+            // Choix : Celsius => Fahrenheit
+            resultValue = toFahrenheit(from: userValue)
+            // On change le texte du resultLabel
+            resultLabel.text = resultValue.description + " °F"
+            break
+        case 1:
+            // Choix : Fahrenheit => Celsius
+            resultValue = toCelsius(from: userValue)
+            // On change le texte du resultLabel
+            resultLabel.text = resultValue.description + " °C"
+            break
+        default:
+            resultLabel.text = "Erreur de direction"        }
+    }
+    
+    // MARK: UIPickerViewDelegate Methods
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerValues.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerValues[row]
+    }
+    
+    
+    // MARK: Convert Methods
+    
+    func toFahrenheit(from celsiusValue: Float) -> Float {
+        // Renvoie la valeur convertie
+        return celsiusValue * 1.8 + 32
+    }
+    
+    func toCelsius(from fahrenheitValue: Float) -> Float {
+        return (fahrenheitValue - 32) / 1.8
+    }
+    
+    // MARK : UITextFieldDelegate Methods
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    // MARK: Override Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set current ViewController as delegate & data source of the directionPicker
+        userDirectionPicker.delegate = self
+        userDirectionPicker.dataSource = self
+        
+        // Set border to result UIView
+        resultView.layer.borderColor = UIColor.black.cgColor
+        resultView.layer.borderWidth = 1
+        
+        // Set default text to empty value
+        resultLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
